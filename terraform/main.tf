7e8047a03350a36f79ecb406d881811b3ede8e03
brandonPurvis <bcpmax@gmail.com>
2019-11-12 19:01:55 -0500

    Initial commit with contributor role.

---
+++ terraform/main.tf
@@ -0,0 +1,42 @@
+provider "aws" {
+  version = "~> 2.0"
+}
+
+
+data "aws_iam_policy_document" "contributor-policy-document" {
+  statement {
+    effect = "Allow"
+    actions = ["*"]
+    resources = ["*"]
+  }
+}
+
+data "aws_iam_policy_document" "contributor-assume-role-document" {
+  statement {
+    effect = "Allow"
+    actions = ["sts:AssumeRole"]
+    principals {
+      identifiers = ["arn:aws:iam::AWS-account-ID:user/user-name"]
+      type = "AWS"
+    }
+  }
+}
+
+
+resource "aws_iam_policy" "contributor-policy" {
+  name = "My-Contributor-Policy"
+  description = "Permit actions for contributors."
+  policy = "${data.aws_iam_policy_document.contributor-policy-document.json}"
+}
+
+
+resource "aws_iam_role" "contributor-role" {
+  name = "My-Contributor-Role"
+  assume_role_policy = "${data.aws_iam_policy_document.contributor-assume-role-document}"
+}
+
+
+resource "aws_iam_role_policy_attachment" "contributor-attachement" {
+  role= "${aws_iam_role.contributor-role.name}"
+  policy_arn = "${aws_iam_policy.contributor-policy.arn}"
+}
